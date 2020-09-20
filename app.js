bf93b8fc47f614bd96bbc78501be4698c79e941a
Voinov Nikita <nikita-nikita2012@mail.ru>
2020-09-20 16:49:24 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const game = new Game();
+game.start();
+
+import loadGame, {Game as loadGame, GameSavingData as loadGame} from './game.js';
+import saveGame, {readGameSaving as saveGame, writeGameSaving as saveGame} from './game.js';
